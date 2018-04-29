4c04dc9eefff36b9311a74989339cb5281572991
AndrewPochapsky <andrew.pochapsky@gmail.com>
2018-04-29 19:00:19 -0400

    fixed bug for getting proper months given a year, program now outpus the top team of a year and month

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #! python3
 
 import bs4, requests, logging
-#logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s- %(message)s')
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s- %(message)s')
+
+logging.disable(logging.CRITICAL)
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
@@ -12,6 +14,7 @@ class Team:
         self.name = name
         self.members = members
 
+#get the top team given a url
 def getTopTeam(url):
     res = requests.get(url, headers = headers)
     res.raise_for_status()
@@ -20,51 +23,81 @@ def getTopTeam(url):
     team = Team(elems[0].text, '')
     return team
 
+#Get all of the possible months given a year
 def getPossibleMonths(year):
-    res = requests.get(baseUrl, headers = headers) # Go to the most recent rankings
+    res = requests.get(baseUrl + '/ranking/teams/', headers = headers) # Go to the most recent rankings
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
-    
+
     divs = soup.find_all("div", {"class": "filter-column-content"}) # find all divs with this class
-    monthsDiv = divs[1] # select the second one as that is the one that holds the months
+    yearsDiv = divs[0]  # select the first one as that is the one that holds the years
+
+    yearLink = ''
+    yearsDivChildren = yearsDiv.find_all('a') # get all of the children of that div
+    for child in yearsDivChildren:
+        if(child.text.strip() == year):
+            yearLink = child['href']
+
+    url = "https://www.hltv.org" + yearLink
+
+    res = requests.get(url, headers = headers) # Go to the rankings of 'year'
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+    divs = soup.find_all("div", {"class": "filter-column-content"}) # find all divs with this class
+    monthsDiv = divs[1] # select the second one as that is the one that holds the months 
+    
     monthsDivChildren = monthsDiv.find_all('a') # get all of the children of that div
 
     monthsDictionary = {} # key = month, value = href
     
     for a in monthsDivChildren:
-        monthsDictionary[a.text] = a['href']
+        monthsDictionary[a.text.strip()] = a['href']
 
     return monthsDictionary
-    
-#body > div.bgPadding > div > div.colCon > div.leftCol > div > div.sidebar-box > div.sidebar-first-level > div > div > div:nth-child(4) > a.sidebar-single-line-item.selected
-#body > div.bgPadding > div > div.colCon > div.leftCol > div > div.sidebar-box > div.sidebar-first-level > div > div > div:nth-child(4) > a:nth-child(2)
-#body > div.bgPadding > div > div.colCon > div.leftCol > div > div.sidebar-box > div.sidebar-first-level > div > div > div:nth-child(4) > a:nth-child(3)
 
-#divTag = soup.find_all("div", {"class": "tablebox"}):
+baseUrl = "https://www.hltv.org" 
 
-#for tag in divTag:
- #   tdTags = tag.find_all("td", {"class": "align-right"})
-  #  for tag in tdTags:
-   #     print tag.text
+possibleYears = ['2018', '2017', '2016', '2015']
 
-baseUrl = "https://www.hltv.org/ranking/teams" # brings you to the most reset rankings
-
-urlWithInsertions = "https://www.hltv.org/ranking/teams/{}/{}/{}" # https://www.hltv.org/ranking/teams/YEAR/MONTH/DAY
-
-possibleYears = [2018, 2017, 2016, 2015]
-
-year = 0000
+year = ''
 month = ""
-day = 00
-months = getPossibleMonths(2018)
 
-for key in months:
-    print(key + ": " + months[key])
-    
+formattedYears = ', '.join(possibleYears)
+
+yearMessage = "Enter a Year | ("+ formattedYears + "): "
+
+userInput = True
+while userInput:
+     year = str(input(yearMessage))
+     if(year not in possibleYears):
+        print("Invalid Year")
+     else:
+        userInput = False
+
+possibleMonths = getPossibleMonths(year)
 
 
-#year = int(input("Year: "))
+formattedMonths = ', '.join(possibleMonths)
+monthMessage = "Enter a Month | (" + formattedMonths + "): "
 
-#month = str(input("Month: "))
-#print("Year: {}, Month: {}".format(year, month))
+userInput = True
+
+while userInput:
+     try:   
+         month = str(input(monthMessage))
+         value = possibleMonths[month]
+         userInput = False # this line only gets executed if key is valid, since otherwise an exception is thrown
+     except:
+         print("Invalid Month")
+
+print("\nTop team of {} in {}: ".format(month, year))
+
+team = getTopTeam(baseUrl + possibleMonths[month])
+print(team.name)
+
+
+
+
+
 
